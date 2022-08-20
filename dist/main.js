a6de54f5104bf0b038940653ddf8ad8594013644
non7247 <funi_funi@nifty.com>
2022-08-20 18:09:09 +0900

    とりあえず反転処理を書いてみたけど動かない状態

--- dist/main.js
+++ dist/main.js
@@ -11,3 +11,42 @@ var ctx = canvas.getContext('2d');
 img.onload = function() {
     ctx.drawImage(img, 0, 0);
 }
+
+var original = function() {
+    console.log("original");
+    ctx.drawImage(img, 0, 0);
+}
+
+var sepia = function() {
+    console.log("sepia");
+}
+
+var invert = function() {
+    console.log("invert");
+    ctx.drawImage(img, 0, 0);
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    invoke('img_invert', { pixels: imageData.data }).then(respose => {
+        imageData.data = respose;
+    });
+    ctx.putImageData(imageData, 0, 0);
+}
+
+var grayscale = function() {
+    console.log("grayscale");
+}
+
+const inputs = document.querySelectorAll('[name=color]');
+for (const input of inputs) {
+    input.addEventListener("change", function(evt) {
+        switch (evt.target.value) {
+        case 'inverted':
+            return invert();
+        case 'grayscale':
+            return grayscale();
+        case 'sepia':
+            return sepia();
+        default:
+            return original();
+        }
+    });
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,27 @@
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![
+        img_invert,
+    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn img_invert(pixels: Vec<u8>) -> Vec<u8> {
+    let mut result: Vec<u8> = Vec::new();
+
+    let mut idx = 0;
+    loop {
+        if idx > pixels.len() - 1 {
+            break;
+        }
+        result.push(255 - pixels[idx]);
+        result.push(255 - pixels[idx + 1]);
+        result.push(255 - pixels[idx + 2]);
+        idx += 4;
+    }
+
+    result
 }
